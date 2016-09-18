dc6b0378cbb45c66e50b211d0921252f65a26835
Vlastimir <vlastimirsd@gmail.com>
2016-09-18 14:56:26 +0000

    added a quote page

---
+++ app/app.js
@@ -0,0 +1,27 @@
+$(document).ready(init);
+
+function init() {
+	// body...
+    getQuote();
+	$('#new-quote').on("click", getQuote);
+}
+
+function getQuote () {
+	var text;
+	var author;
+
+    $.getJSON("http://api.forismatic.com/api/1.0/?format=jsonp&method=getQuote&jsonp=?&lang=en", function(json) {
+		text = json.quoteText;
+		 author = json.quoteAuthor;
+        
+		$('#qoute-text').text('"' + text + '"');
+
+		 if (author == "") {
+		 	$('#quote-author').text('- No author -');
+		 } else {
+			 $('#quote-author').text('- ' + author + ' -');
+		 }
+         $('#tweetQuote').attr('href', 'https://twitter.com/intent/tweet?text=' + text).attr('target', '_blank');
+
+    });
+}
